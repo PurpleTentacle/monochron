1ed3df1072f466e9fe1599de9e4882ea0114e415
PurpleTentacle <fabianms@web.de>
2010-10-24 21:02:19 +0200

    precompiled DCF77 binaries added

---
+++ firmware/distribution/loadFWdcf.bat
@@ -0,0 +1,1 @@
+avrdude -p atmega328p -c usbtiny -B 1 -U flash:w:MultiChronDCF.hex -U eeprom:w:MultiChronDCF.eep
---
+++ firmware/distribution/loadFWdcf.sh
@@ -0,0 +1,1 @@
+sudo avrdude -p atmega328p -c usbtiny -B 1 -U flash:w:MultiChronDCF.hex -U eeprom:w:MultiChronDCF.eep
